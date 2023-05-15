34a3fed545d65cb31907cf13942e777363238c34
alindrapu <alindrapu@gmail.com>
2023-05-15 11:09:25 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h3 className="flex bg-slate-400 bg-gradient-to-r from-slate-500 to-slate-300">
+      welcome to secrlikee
+    </h3>
+  );
+}
+
+export default App;
